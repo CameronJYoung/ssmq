f2419100bd02f666561696994f1f8a91b074c45b
cameronjyoung <cameronjyoung123@gmail.com>
2024-04-21 21:05:09 +0100

    Complete basic daemon setup

--- daemon/src/main.rs
+++ daemon/src/main.rs
@@ -3,32 +3,18 @@ use daemonize::Daemonize;
 use std::fs::File;
 
 use crate::interface::cli_server::CliServer;
+use crate::system::setup_daemon::setup_daemon;
 
 mod interface;
+mod system;
 
 #[tokio::main]
 async fn main() {
-    let stdout = File::create("/tmp/ssmqd.out").unwrap();
-    let stderr = File::create("/tmp/ssmqd.err").unwrap();
-
-    let daemonize = Daemonize::new()
-        .pid_file("/tmp/ssmqd.pid")
-        .chown_pid_file(true)
-        .working_directory("/tmp")
-        .user("nobody")
-        .group("daemon")
-        .stdout(stdout)
-        .stderr(stderr);
-
-    match daemonize.start() {
-        Ok(_) => println!("Daemon started successfully"),
-        Err(e) => eprintln!("Error starting daemon: {}", e),
-    }
-
-    println!("Starting SSMQ daemon...");
+    setup_daemon();
 
     println!("Starting CLI server...");
     let cli_server = Arc::new(CliServer::new("127.0.0.1".to_string(), 8080));
+
     let cli_server_handle = {
         let cli_server = Arc::clone(&cli_server);
         tokio::spawn(async move {
---
+++ daemon/src/system/setup_daemon.rs
@@ -0,0 +1,23 @@
+use std::fs::File;
+use daemonize::Daemonize;
+
+pub fn setup_daemon() {
+    println!("Starting SSMQ daemon...");
+
+    let log_file = File::create("/var/log/ssmq.log").unwrap();
+    let err_log_file = log_file.try_clone().expect("Unable to clone log file for stderr");
+
+    let daemonize = Daemonize::new()
+        .pid_file("/var/run/ssmq.pid")
+        .chown_pid_file(true)
+        .working_directory("/var/lib/ssmq")
+        .user("nobody")
+        .group("daemon")
+        .stdout(log_file)
+        .stderr(err_log_file);
+
+    match daemonize.start() {
+        Ok(_) => println!("Daemon started successfully"),
+        Err(e) => eprintln!("Error starting daemon: {}", e),
+    }
+}
