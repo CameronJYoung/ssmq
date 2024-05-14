3021098462162b418d57f3433c6f90f1a3a88d68
cameronjyoung <cameronjyoung123@gmail.com>
2024-05-14 01:29:54 +0100

    Add status check for cli

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,4 +1,6 @@
 use clap::Command;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpStream;
 
 #[tokio::main]
 async fn main() {
@@ -12,7 +14,7 @@ async fn main() {
             println!("Stopping SSMQ daemon...");
         },
         Some("status") => {
-            println!("Checking SSMQ daemon status...");
+            ssmq_status_check().await;
         },
         _ => {
             println!("Invalid command. Use 'ssmq-cli --help' for more information.");
@@ -39,4 +41,15 @@ fn cli() -> Command {
             Command::new("status")
                 .about("Returns the status of the SSMQ daemon")
         )
+}
+
+async fn ssmq_status_check() {
+    let mut tcp_client = TcpStream::connect("127.0.0.1:8080").await.unwrap();
+
+    let mut buf = vec![0; 1024];
+    tcp_client.write_all(b"status\n").await.unwrap();
+    let n = tcp_client.read(&mut buf).await.unwrap();
+    let response = String::from_utf8_lossy(&buf[..n]);
+
+    println!("{}", response);
 }
