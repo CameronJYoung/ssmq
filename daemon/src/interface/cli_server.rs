18cf20453079d0c2f0a4b96e98662d521aa87bcc
cameronjyoung <cameronjyoung123@gmail.com>
2024-05-14 01:30:30 +0100

    Add logging

--- daemon/src/interface/cli_server.rs
+++ daemon/src/interface/cli_server.rs
@@ -25,25 +25,28 @@ impl CliServer {
     }
 
     pub async fn start(&self) -> Result<(), Box<dyn Error>> {
+        println!("Starting CLI Server on {}:{}", self.address, self.port);
         let listener = TcpListener::bind(format!("{}:{}", self.address, self.port)).await?;
         println!("CLI Server running on {}:{}", self.address, self.port);
 
         while !self.should_stop.load(Ordering::SeqCst) {
+            println!("Waiting for a new connection...");
             tokio::select! {
-                Ok((stream, _)) = listener.accept() => {
-                    let stop_clone = self.should_stop.clone();
-                    let stopped_clone = self.notify_stopped.clone();
-                    tokio::spawn(async move {
-                        if let Err(e) = Self::handle_connection(stream, stop_clone).await {
-                            eprintln!("CLI Connection closed: {}", e);
-                        }
-                        stopped_clone.notify_one(); // Notify that one connection is closed
-                    });
-                },
-                _ = self.notify_stop.notified() => {
-                    break; // Break out of the loop when notified to stop
-                },
-            }
+            Ok((stream, _)) = listener.accept() => {
+                println!("Hello");
+                let stop_clone = self.should_stop.clone();
+                let stopped_clone = self.notify_stopped.clone();
+                tokio::spawn(async move {
+                    if let Err(e) = Self::handle_connection(stream, stop_clone).await {
+                        eprintln!("CLI Connection closed: {}", e);
+                    }
+                    stopped_clone.notify_one(); // Notify that one connection is closed
+                });
+            },
+            _ = self.notify_stop.notified() => {
+                break; // Break out of the loop when notified to stop
+            },
+        }
         }
 
         println!("CLI server is shutting down");
@@ -60,11 +63,14 @@ impl CliServer {
     async fn handle_connection(mut stream: TcpStream, should_stop: Arc<AtomicBool>) -> Result<(), Box<dyn Error>> {
         let mut buf = vec![0; 1024];
 
+        println!("connection established");
+
         while !should_stop.load(Ordering::SeqCst) {
             match stream.read(&mut buf).await {
                 Ok(n) if n == 0 => return Err("Connection closed by client".into()),
                 Ok(n) => {
                     let received = String::from_utf8_lossy(&buf[..n]);
+                    println!("Received: {}", received.trim());
                     match received.trim() {
                         "status" => {
                             let response = "Daemon is running\n";
