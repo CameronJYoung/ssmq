59f0e6528d9ca63a8ae7b583ab52d9aba1f50532
cameronjyoung <cameronjyoung123@gmail.com>
2024-05-14 01:30:19 +0100

    Split tokio main from daemonize main

--- daemon/src/main.rs
+++ daemon/src/main.rs
@@ -1,4 +1,5 @@
 use std::sync::Arc;
+use tokio::io;
 
 use crate::interface::cli_server::CliServer;
 use crate::system::setup_daemon::setup_daemon;
@@ -6,22 +7,20 @@ use crate::system::setup_daemon::setup_daemon;
 mod interface;
 mod system;
 
-#[tokio::main]
-async fn main() {
+fn main() {
     setup_daemon();
 
+    tokio_main();
+}
+
+#[tokio::main]
+async fn tokio_main() {
     println!("Starting CLI server...");
     let cli_server = Arc::new(CliServer::new("127.0.0.1".to_string(), 8080));
 
-    let cli_server_handle = {
-        let cli_server = Arc::clone(&cli_server);
-        tokio::spawn(async move {
-            match cli_server.start().await {
-                Ok(_) => println!("CLI server started successfully"),
-                Err(e) => eprintln!("Failed to start CLI server: {}", e)
-            }
-        })
-    };
+    if let Err(e) = cli_server.start().await {
+        eprintln!("Error starting CLI server: {}", e);
+    }
 
-    cli_server_handle.await.unwrap();
+    println!("CLI server stopped");
 }
